20a1b34f5c85bc579285b0acf3cb89d6de66e14d
Samantha Barros <samantha.barros95@gmail.com>
2020-02-23 00:38:10 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,8 +2,8 @@ def oxford_comma(array)
   if array.length ==2 
     array.join(" and ")
   elsif array.length > 2 
-    array[-1].include " and"
-    
+    array[-1].include "and"
+    array.join(" ,")
   else
     array.join(" ")
   end
