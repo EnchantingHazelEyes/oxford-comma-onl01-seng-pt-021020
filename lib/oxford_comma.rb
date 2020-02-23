117be4c5e01cea4e9ffe5bc84a475d63c6f1c8ec
Samantha Barros <samantha.barros95@gmail.com>
2020-02-23 00:40:54 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
     array.join(" and ")
   elsif array.length > 2 
     array[-1].prepend " and "
-    array.join(",")
+    array.join(" ,")
   else
     array.join(" ")
   end
